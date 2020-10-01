38d769813f4fa3a29380a80b1d5cf863e4c14ba9
Mauro Avellaneda <mauroeavellaneda@gmail.com>
2020-10-01 12:03:19 +0200

    unsuccessfully trying to render the mapping of 'searches'

--- src/components/DisplaySearches.jsx
+++ src/components/DisplaySearches.jsx
@@ -1,23 +1,34 @@
-import React, { Component } from 'react'
-import { fetchGhData } from '../modules/fetchGhData'
+import React, { Component } from "react";
+import { fetchGhData } from "../modules/fetchGhData";
 
 class DisplaySearches extends Component {
-    state = {
-        searches: [],
-    }
+  state = {
+    searches: [],
+  };
 
-    componentDidMount = async () => {
-        const searches = await fetchGhData();
-        this.setState({searches: searches})
-    }
+  componentDidMount = async () => {
+    const searches = await fetchGhData();
+    this.setState({ searches: searches });
+    debugger
+  };
 
-    render() {
+  render() {
+    let searches;
+    if (this.state.searches !== []) {
+      searches = this.state.searches.map((search) => {
         return (
-            <div id='displaysearches'>GH users searched
-                
-            </div>
-        )
+          <>
+            <div>{this.state.searches}</div>
+          </>
+        );
+      });
     }
+    return (
+        <div id='displaysearches'>
+        {searches}
+        </div>
+    )
+  }
 }
 
-export default DisplaySearches
+export default DisplaySearches;
