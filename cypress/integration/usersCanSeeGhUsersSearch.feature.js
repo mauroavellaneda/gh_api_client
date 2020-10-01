3c9adfeef1e7b01e9316dddafb62e8290da9ca2c
Mauro Avellaneda <mauroeavellaneda@gmail.com>
2020-10-01 10:52:40 +0200

    Adds feature test for 'users can see GH search registry

---
+++ cypress/integration/usersCanSeeGhUsersSearch.feature.js
@@ -0,0 +1,15 @@
+describe('Users can see GH search', () => {
+    beforeEach(() => {
+        cy.server();
+        cy.route({
+            method: "GET",
+            url: "http://localhost:3000/api/v1/products",
+            response: "fixture:users_search_index.json",
+
+        });
+        cy.visit("/");
+    })
+    it('user can see list of searches', () => {
+        cy.get(#'display-searches').should('exist')
+    } )
+})
